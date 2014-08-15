4f59ce5f6f68833d87d18e816063ebe535042295
hitakaken <hitakaken@gmail.com>
2014-08-15 18:40:03 +0800

    Initial Commit

--- karaf-features-plugin/src/main/groovy/bigfoot/framework/gradle/plugin/KarafFeatureRepositoryExtension.groovy
+++
@@ -1,8 +0,0 @@
-package bigfoot.framework.gradle.plugin
-
-/**
- * Created by Owner on 2014/8/14.
- */
-class KarafFeatureRepositoryExtension {
-    String repos = [];
-}
--- karaf-features-plugin/src/main/groovy/bigfoot/framework/gradle/plugin/karafFeaturesPlugin.groovy
+++ karaf-features-plugin/src/main/groovy/bigfoot/framework/gradle/plugin/karafFeaturesPlugin.groovy
@@ -15,8 +15,13 @@
  */
 package bigfoot.framework.gradle.plugin
 
+import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.dsl.DependencyHandler
+import org.gradle.api.internal.artifacts.dsl.dependencies.DefaultDependencyHandler
+import org.gradle.api.specs.Specs
+import org.gradle.api.tasks.TaskAction
 
 /**
  * Created by Owner on 2014/8/14.
@@ -24,6 +29,27 @@ import org.gradle.api.Project
 class KarafFeaturesPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        project.extensions.create("KarafFeatureRepository",KarafFeatureRepositoryExtension)
+        project.extensions.create("karaf_feature_repos",DefaultDependencyHandler)
+        //project.extensions.create("features")
+        project.task("list_features",type:ListFeatureTask)
+    }
+
+    class ListFeatureTask extends DefaultTask{
+        public ListFeatureTask() {
+            getOutputs().upToDateWhen(Specs.satisfyNone());
+        }
+        /**
+         *
+         */
+        @TaskAction
+        def doExecuteTask() {
+            DependencyHandler dependencyHandler = project.extensions.findByName("karaf_feature_repos");
+            def result = dependencyHandler.createArtifactResolutionQuery().execute();
+            for(component in result.resolvedComponents){
+                println component
+            }
+        }
     }
 }
+
+
